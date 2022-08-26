4c3f63337570b3b78ef76473ed41c71b620f1b5e
0x4nz0 <4nz0@proton.me>
2022-08-26 15:15:15 +0200

    fix(Exchange): fix IERC20 balanceOf modifier

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 interface IERC20 {
-    function balanceOf(address) external returns (uint256);
+    function balanceOf(address) external view returns (uint256);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
 
