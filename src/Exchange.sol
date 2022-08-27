8ae57a8da5810e096c740ef49e38f8777765c6ca
0x4nz0 <4nz0@proton.me>
2022-08-27 16:13:52 +0200

    refactor(exchange): add correct pricing function getAmount

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -24,9 +24,29 @@ contract Exchange {
         return IERC20(tokenAddress).balanceOf(address(this));
     }
 
-    function getPrice(uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
+    function getAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve)
+        private
+        pure
+        returns (uint256)
+    {
         require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
 
-        return (inputReserve * 1000) / outputReserve;
+        return ((inputAmount * outputReserve) * 1000) / (inputReserve + inputAmount);
+    }
+
+    function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
+        require(_ethSold > 0, "ethSold is too small");
+
+        uint256 tokenReserve = getReserve();
+
+        return getAmount(_ethSold, address(this).balance, tokenReserve);
+    }
+
+    function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
+        require(_tokenSold > 0, "tokenSold is too small");
+
+        uint256 tokenReserve = getReserve();
+
+        return getAmount(_tokenSold, tokenReserve, address(this).balance);
     }
 }
