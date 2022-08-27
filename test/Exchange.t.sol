3ea112f337ac6fe0828bcdc5264589366552a807
0x4nz0 <4nz0@proton.me>
2022-08-27 15:21:59 +0200

    test(exchange): add testPrice

--- test/Exchange.t.sol
+++ test/Exchange.t.sol
@@ -25,4 +25,18 @@ contract ExchangeTest is Test {
         assertEq(address(exchange).balance, 100 wei);
         assertEq(exchange.getReserve(), 200 wei);
     }
+
+    function testPrice() public {
+        token.approve(address(exchange), 2000 wei);
+        exchange.addLiquidity{value: 1000 wei}(2000 wei);
+
+        uint256 tokenReserve = exchange.getReserve();
+        uint256 etherReserve = address(exchange).balance;
+
+        // ETH per Token
+        assertEq(exchange.getPrice(etherReserve, tokenReserve), 500); // (1000 * 1000) / 2000 = 500
+
+        // Token per ETH
+        assertEq(exchange.getPrice(tokenReserve, etherReserve), 2000); // (2000 * 1000) / 1000 = 20000
+    }
 }
