a22f56e3b568b98647d991eb80c7dc1bd7f7c627
0x4nz0 <4nz0@proton.me>
2022-08-30 11:54:28 +0200

    fix(exchange): update testGetEthAmount and testGetTokenAmount

--- test/Exchange.t.sol
+++ test/Exchange.t.sol
@@ -89,13 +89,13 @@ contract ExchangeTest is Test {
         exchange.addLiquidity{value: 1000 wei}(2000 wei);
 
         uint256 tokensOut = exchange.getTokenAmount(1 wei);
-        assertEq(tokensOut, 1998); // ((1 * 2000) * 1000) / (1 + 1000)
+        assertEq(tokensOut, 1); // ((1 * 99) * 2000) / ((1000 * 100) + (1 * 99)) = 1,978041739
 
         tokensOut = exchange.getTokenAmount(100 wei);
-        assertEq(tokensOut, 181818); // ((100 * 2000) * 1000) / (100 + 1000)
+        assertEq(tokensOut, 180); // ((100 * 99) * 2000) / ((1000 * 100) + (100 * 99)) = 180,163785259
 
         tokensOut = exchange.getTokenAmount(1000 wei);
-        assertEq(tokensOut, 1000000); // ((1000 * 2000) * 1000) / (1000 + 1000)
+        assertEq(tokensOut, 994); // ((1000 * 99) * 2000) / ((1000 * 100) + (1000 * 99)) = 994,974874372
     }
 
     function testGetEthAmount() public {
@@ -103,12 +103,12 @@ contract ExchangeTest is Test {
         exchange.addLiquidity{value: 1000 wei}(2000 wei);
 
         uint256 ethOut = exchange.getEthAmount(2 wei);
-        assertEq(ethOut, 999); // ((2 * 1000) * 1000) / (2 + 2000)
+        assertEq(ethOut, 0); // ((2 * 99) * 1000) / ((2000 * 100) + (2 * 99)) = 0,989020869
 
         ethOut = exchange.getEthAmount(100 wei);
-        assertEq(ethOut, 47619); // ((100 * 1000) * 1000) / (100 + 2000)
+        assertEq(ethOut, 47); // ((100 * 99) * 1000) / ((2000 * 100) + (100 * 99)) = 47,165316818
 
         ethOut = exchange.getEthAmount(2000 wei);
-        assertEq(ethOut, 500000); // ((2000 * 1000) * 1000) / (2000 + 2000)
+        assertEq(ethOut, 497); // ((2000 * 99) * 1000) / ((2000 * 100) + (2000 * 99)) = 497,487437186
     }
 }
