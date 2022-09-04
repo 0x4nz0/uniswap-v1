031dd6bb7b061fe056e939d2d2125991e2c60c16
0x4nz0 <4nz0@proton.me>
2022-09-04 10:17:36 +0200

    feat(factory): add createExchange

--- src/Factory.sol
+++ src/Factory.sol
@@ -5,4 +5,14 @@ import "./Exchange.sol";
 
 contract Factory {
     mapping(address => address) public tokenToExchange;
+
+    function createExchange(address _tokenAddress) public returns (address) {
+        require(_tokenAddress != address(0), "invalid token address");
+        require(tokenToExchange[_tokenAddress] == address(0), "exchange already exists");
+
+        Exchange exchange = new Exchange(_tokenAddress);
+        tokenToExchange[_tokenAddress] = address(exchange);
+
+        return address(exchange);
+    }
 }
